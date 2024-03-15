2581b3299d9cc7ce99d41ad5154165310758506d
GiteshDewangan <giteshdewangan123456@gmail.com>
2024-03-15 11:59:40 +0530

    rentRoverhub

---
+++ src/components/LandingPageHeader/index.jsx
@@ -0,0 +1,97 @@
+import React from "react";
+
+import { Button, Img, List, Text } from "components";
+import { NavLink,Link, useNavigate } from "react-router-dom";
+const LandingPageHeader = (props) => {
+const nav=useNavigate();
+  return (
+    <>
+      <header className={props.className}>
+        <div className="flex md:flex-col flex-row md:gap-10 items-center justify-between w-full">
+          <div className="header-row my-px">
+            <div className="flex flex-row gap-[11px] items-center justify-start">
+              <Img className="h-10 w-10" src="
+              https://media.istockphoto.com/id/1211788306/photo/simple-white-house-icon-3d-rendering.webp?s=1024x1024&w=is&k=20&c=p6ATwfbKtz4EEuonXHAlCgnZbHEvOsU4ucKtOqJVGvw=" alt="home" />
+              <Text
+                className="text-orange-A700 text-xl w-auto"
+                size="txtMarkoOneRegular20"
+              >
+                RentRoverHub
+              </Text>
+            </div>
+            <div className="mobile-menu">
+              <div></div>
+              <div></div>
+              <div></div>
+            </div>
+          </div>
+          <div className="flex sm:flex-1 sm:flex-col flex-row sm:hidden items-center justify-between w-[492px] sm:w-full">
+            <List
+              className="sm:flex-col flex-row gap-10 grid grid-cols-3"
+              orientation="horizontal"
+            >
+              <div className="flex flex-row gap-1.5 items-start justify-start w-[77px]">
+                <Text
+                  className="text-base text-gray-900 w-auto"
+                  size="txtManropeSemiBold16"
+                >
+                  <NavLink to='/'>Home</NavLink>
+                </Text>
+                
+              </div>
+
+              <div className="flex flex-row gap-1.5 items-start justify-start w-[77px]">
+                <Text
+                  className="text-base text-gray-900 w-auto"
+                  size="txtManropeSemiBold16"
+                >
+                  <NavLink to='/listing'>Listing</NavLink>
+                </Text>
+                
+              </div>
+
+              <div className="flex flex-row gap-1.5 items-start justify-start w-[77px]">
+                <Text
+                  className="text-base text-gray-900 w-auto"
+                  size="txtManropeSemiBold16"
+                >
+                  <NavLink to='/agentlist'>Agents</NavLink>
+                </Text>
+                
+              </div>
+              
+            </List>
+            <Text
+              className="text-base text-center text-gray-900 w-auto"
+              size="txtManropeSemiBold16"
+            >
+            <NavLink to='/propertydetails'>property</NavLink>
+            </Text>
+            <Text
+              className="text-base text-gray-900 w-auto"
+              size="txtManropeSemiBold16"
+            >
+              <NavLink to='/blogdetails'>Blogs</NavLink>
+            </Text>
+            <Text
+              className="text-base text-gray-900 w-auto"
+              size="txtManropeSemiBold16"
+            >
+              <NavLink to='/aboutus'>About</NavLink>
+            </Text>
+          </div>
+          <div className="flex flex-row gap-10 h-[42px] md:h-auto sm:hidden items-center justify-start w-[228px]">
+            <input type="text" placeholder="Search" style={{width:"100px",borderRadius:"10px"}}/>
+            <Button onClick={()=>nav('/login')} className="bg-gray-900 cursor-pointer font-manrope font-semibold py-2.5 rounded-[10px] text-base text-center text-white-A700 w-full">
+              Log in
+            </Button>
+          </div>
+        </div>
+      </header>
+    </>
+  );
+};
+
+LandingPageHeader.defaultProps = {};
+
+export default LandingPageHeader;
